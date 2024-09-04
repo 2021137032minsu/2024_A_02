7c2a5c4d95fcd61c96da5352d32f2334ff865d9a
9alstn <163296030+2021137032minsu@users.noreply.github.com>
2024-09-04 16:21:06 +0800

    Update MoveTest.cs

--- Assets/Scripts/MoveTest.cs
+++ Assets/Scripts/MoveTest.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveTest : MonoBehaviour
+public class Movetest : MonoBehaviour
 {
-    public Transform Move;
+    public Vector3 move;
     // Start is called before the first frame update
     void Start()
     {
